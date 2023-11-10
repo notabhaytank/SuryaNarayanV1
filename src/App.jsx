3e1eaa93ef1ae9dfa2adc6088f286077a67be394
Abhay <notabhaytank@gmail.com>
2023-11-10 07:45:07 +0530

    Version2

--- src/App.jsx
+++ src/App.jsx
@@ -9,65 +9,117 @@ import Work from "./sections/Work.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footers from "./sections/Footers.jsx";
 import WhatOthersSay from "./sections/What-others-say.jsx";
-import toogle from './assets/tooglewhite.svg'
+import Toggle from './assets/tooglewhite.svg'
+import DarkToggle from './assets/dardtoogke.svg'
 
 function App() {
   const [count, setCount] = useState(0)
   const [theme, setTheme] = useState("Light")
 
-  useEffect(( ) => {
-        if (theme === "dark"){
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark")
-        }
-  }, [theme]);
 
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [theme]);
 
     const handleThemeSwitch = () => {
-    setTheme(theme=== "dark" ? "light" : "dark");
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+
   };
 
 
+//
+//   return (
+//     < >
+//         <div className="items-start bg-white flex flex-col   ">
+//
+//
+//
+//
+//             <Header/>
+//
+//             <header className="justify-center fixed dark:bg-black z-50 items-center  self-stretch border-b-[color:var(--light-mode-stroke,#D7DDE4)] bg-white flex w-full flex-col h-12 px-5 border-b  border-solid max-lg:max-w-full  max-lg:px-[16px]">
+//                 <div className="self-center dark:bg-black  h-12  w-[358px]  lg:w-[1200px]   ">
+//                     <div className="flex dark:bg-black   max-lg:gap-0">
+//
+//                         <div className={`flex ${theme === 'dark' ? 'bg-black' : 'bg-white'} justify-center items-start px-4 lg:px-10 border-l h-12 border-solid border-r-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)] border-r border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] flex-col w-[358px] lg:w-4/12 max-lg:w-full max-lg:ml-0`}>
+//                             <span onClick={handleThemeSwitch}>
+//                                 <img src={Toggle} className="h-7 align-middle" alt="Toggle" />
+//                                 {theme === 'dark' ? 'Dark Mode' : 'Light Mode'}
+//                                 {theme === 'dark' ? <DarkToggle /> : <Toggle />}
+//                             </span>
+//                         </div>
+//
+//                         <div className="hidden dark:bg-black lg:flex justify-end border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] self-stretch  bg-white  h-12 flex-col mx-auto  border-solid items-stretch w-3/12 max-lg:w-full max-lg:ml-0"></div>
+//                         <div className="hidden dark:bg-black lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)] bg-white  h-12 mx-auto border-l border-solid flex-col items-stretch w-1/12 max-lg:w-full max-lg:ml-0"></div>
+//                         <div className="hidden dark:bg-black lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-r-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)]  h-12 flex-col mx-auto border-r border-solid border-l items-stretch w-4/12 "></div>
+//                     </div>
+//                 </div>
+//             </header>
+//             <Name/>
+//             <Intro/>
+//             <Work/>
+//             <WhatOthersSay/>
+//             <Contact/>
+//             <Footers/>
+//
+//         </div>
+//
+//
+//
+//
+//     </>
+//   )
+// }
+//
+// export default App
 
-  return (
-    < >
-        <div className="items-start bg-white flex flex-col   ">
 
 
 
 
-            <Header/>
 
-            <header className="justify-center fixed dark:bg-black z-50 items-center  self-stretch border-b-[color:var(--light-mode-stroke,#D7DDE4)] bg-white flex w-full flex-col h-12 px-5 border-b  border-solid max-lg:max-w-full  max-lg:px-[16px]">
-                <div className="self-center dark:bg-black  h-12  w-[358px]  lg:w-[1200px]   ">
-                    <div className="flex dark:bg-black   max-lg:gap-0">
 
-                        <div  className="flex dark:bg-black justify-center items-start px-4 lg:px-10 border-l  h-12 border-solid  border-r-[color:var(--light-mode-stroke,#D7DDE4)]  border-l-[color:var(--light-mode-stroke,#D7DDE4)] border-r border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] bg-white flex-col  w-[358px] lg:w-4/12 max-lg:w-full max-lg:ml-0">
 
-                            <img onClick={handleThemeSwitch} src={toogle} className="h-7 align-middle " />
 
+
+
+    return (
+        <>
+            <div className="items-start bg-white flex flex-col   ">
+                {/* Rest of your code */}
+                <header className="justify-center fixed dark:bg-black z-50 items-center  self-stretch border-b-[color:var(--light-mode-stroke,#D7DDE4)] bg-white flex w-full flex-col h-12 px-5 border-b  border-solid max-lg:max-w-full  max-lg:px-[16px]">
+                    {/* Rest of your header content */}
+                    <div className="self-center dark:bg-black  h-12  w-[358px]  lg:w-[1200px]   ">
+                        <div className="flex dark:bg-black   max-lg:gap-0">
+                                    <div
+                                        className={`flex ${theme === 'dark' ? 'bg-black' : 'bg-white'} justify-center items-start px-4 lg:px-10 border-l h-12 border-solid border-r-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)] border-r border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] flex-col w-[358px] lg:w-4/12 max-lg:w-full max-lg:ml-0`}
+                                    >
+                                        <span onClick={handleThemeSwitch}>
+                                          {theme === 'dark' ? <img src={DarkToggle} className="h-7 align-middle" alt="Dark Mode" /> : <img src={Toggle} className="h-7 align-middle" alt="Light Mode" />}
+
+                                        </span>
+                                    </div>
+                                    <div className="hidden dark:bg-black lg:flex justify-end border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] self-stretch  bg-white  h-12 flex-col mx-auto  border-solid items-stretch w-3/12 max-lg:w-full max-lg:ml-0"></div>
+                                    <div className="hidden dark:bg-black lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)] bg-white  h-12 mx-auto border-l border-solid flex-col items-stretch w-1/12 max-lg:w-full max-lg:ml-0"></div>
+                                    <div className="hidden dark:bg-black lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-r-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)]  h-12 flex-col mx-auto border-r border-solid border-l items-stretch w-4/12 "></div>
                         </div>
-                        <div className="hidden dark:bg-black lg:flex justify-end border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] self-stretch  bg-white  h-12 flex-col mx-auto  border-solid items-stretch w-3/12 max-lg:w-full max-lg:ml-0"></div>
-                        <div className="hidden dark:bg-black lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)] bg-white  h-12 mx-auto border-l border-solid flex-col items-stretch w-1/12 max-lg:w-full max-lg:ml-0"></div>
-                        <div className="hidden dark:bg-black lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-r-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)]  h-12 flex-col mx-auto border-r border-solid border-l items-stretch w-4/12 "></div>
                     </div>
-                </div>
-            </header>
-            <Name/>
-            <Intro/>
-            <Work/>
-            <WhatOthersSay/>
-            <Contact/>
-            <Footers/>
-
-        </div>
-
-
-
-
-    </>
-  )
+                    {/* Rest of your header content */}
+                </header>
+                <Name/>
+                <Intro/>
+                <Work/>
+                <WhatOthersSay/>
+                <Contact/>
+                <Footers/>
+                {/* Rest of your code */}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
