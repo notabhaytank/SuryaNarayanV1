e82fca83af35b7b487ac037c436b442b370faa40
Abhay <notabhaytank@gmail.com>
2023-11-10 06:55:42 +0530

    Version2

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -6,10 +6,10 @@ const Header = (props) => (
         <div className="self-center  h-12  w-[358px]  lg:w-[1200px]   ">
             <div className="flex  max-lg:gap-0">
 
-                <div className="flex border-l  h-12 border-solid  border-r-[color:var(--light-mode-stroke,#D7DDE4)]  border-l-[color:var(--light-mode-stroke,#D7DDE4)] border-r border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] bg-[linear-gradient(135deg,#EDF0F4_0%,#FFF_50%,#FFF_100%)] flex-col  w-[358px] lg:w-4/12 max-lg:w-full max-lg:ml-0">
-                    <div className=>
-                    <img src={toogle} className=/>
-                    </div>
+                <div className="flex justify-center items-start px-4 lg:px-10 border-l  h-12 border-solid  border-r-[color:var(--light-mode-stroke,#D7DDE4)]  border-l-[color:var(--light-mode-stroke,#D7DDE4)] border-r border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] bg-[linear-gradient(135deg,#EDF0F4_0%,#FFF_50%,#FFF_100%)] flex-col  w-[358px] lg:w-4/12 max-lg:w-full max-lg:ml-0">
+
+                    <img src={toogle} className="h-7 align-middle " />
+
                 </div>
                 <div className="hidden lg:flex justify-end border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] self-stretch  bg-white  h-12 flex-col mx-auto  border-solid items-stretch w-3/12 max-lg:w-full max-lg:ml-0"></div>
                 <div className="hidden lg:flex self-stretch border-b border-b-[color:var(--light-mode-stroke,#D7DDE4)] border-l-[color:var(--light-mode-stroke,#D7DDE4)] bg-white  h-12 mx-auto border-l border-solid flex-col items-stretch w-1/12 max-lg:w-full max-lg:ml-0"></div>
